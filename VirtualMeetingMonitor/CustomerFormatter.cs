2591f3c0144d6395581a56197f869e9062dce1ea
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2021-10-14 06:20:31 -0300

    small fix

--- VirtualMeetingMonitor/CustomerFormatter.cs
+++ VirtualMeetingMonitor/CustomerFormatter.cs
@@ -17,25 +17,6 @@ namespace VirtualMeetingMonitor
         {
             Functions = functions;
         }
-
-        private string Teste()
-        {
-            return "";
-        }
-        private string Evaluator(List<KeyValuePair<string, string>> map, Match match)
-        {
-            for (int i = 0; i < match.Groups.Count; i++)
-            {
-                var group = match.Groups[i];
-                if (group.Success)
-                {
-                    return map[i].Value;
-                }
-            }
-
-            //shouldn't happen
-            throw new ArgumentException("Match found that doesn't have any successful groups");
-        }
         public string Format(string format)
         {
             var pattern = @"\[(.*?)\]";
@@ -49,7 +30,7 @@ namespace VirtualMeetingMonitor
                 Func<string> showMethod = Functions.Find((inv) => inv.Identificator.ToUpper() == m.Groups[1].ToString().ToUpper()).Method ;
 
                 replacements.Add(m.Groups[1].ToString(), showMethod());
-               Console.WriteLine($"{m.Groups[1]} {showMethod()}");
+           //    Console.WriteLine($"{m.Groups[1]} {showMethod()}");
             }
             // var regex = new Regex(String.Join(pattern, map.Keys));
 
