ae4cddf4fd5d5c96aff899500beb0a28e1fee3c7
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2021-10-18 12:05:42 -0300

    new api integration

---
+++ VirtualMeetingMonitor/Forms/Workshop/UserControllers/Login.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace VirtualMeetingMonitor.Forms.Workshop.UserControllers
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txb_email = new System.Windows.Forms.TextBox();
+            this.txb_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txb_email
+            // 
+            this.txb_email.Location = new System.Drawing.Point(113, 41);
+            this.txb_email.Name = "txb_email";
+            this.txb_email.Size = new System.Drawing.Size(292, 23);
+            this.txb_email.TabIndex = 0;
+            // 
+            // txb_password
+            // 
+            this.txb_password.Location = new System.Drawing.Point(113, 106);
+            this.txb_password.Name = "txb_password";
+            this.txb_password.Size = new System.Drawing.Size(292, 23);
+            this.txb_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txb_password);
+            this.Controls.Add(this.txb_email);
+            this.Name = "Login";
+            this.Size = new System.Drawing.Size(504, 181);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txb_email;
+        private System.Windows.Forms.TextBox txb_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ VirtualMeetingMonitor/Routes.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace VirtualMeetingMonitor
+{
+    internal class Routes
+    {
+        private readonly string url = "http://192.168.0.25"; //esp 8266 fixed ip
+
+        public bool TurnOn(int hue, int sat) => CallAPI($"{{ \"on\": true, \"hue\" : {hue}, \"sat\": {sat} }}") == HttpStatusCode.OK;
+        public bool RunTest() => CallTeste() == HttpStatusCode.OK;
+
+        public bool TurnOff() => CallAPI("{ \"on\": false }") == HttpStatusCode.OK;
+        //public bool TurnOn() => CallAPI("{ \"status\": true }") == HttpStatusCode.OK;
+
+        //public bool TurnOff() => CallAPI("{ \"status\": false }") == HttpStatusCode.OK;
+        private HttpStatusCode CallTeste()
+        {
+
+            RestClient client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            return client.Execute(request).StatusCode;
+        }
+        private HttpStatusCode CallAPI(string body)
+        {
+
+            RestClient client = new RestClient(url);
+
+            const string api = "/LED";
+            var request = new RestRequest(api, Method.POST);
+
+            request.AddJsonBody(body);
+            return client.Execute(request).StatusCode;
+        }
+    }
+}
